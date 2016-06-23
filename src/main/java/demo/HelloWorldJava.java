dff27976dafb3a7c979966812dcf12218bc16103
Alphonse Bendt <alphonse.bendt@gmail.com>
2016-06-23 17:45:52 +0200

    initial

---
+++ src/main/java/demo/HelloWorldJava.java
@@ -0,0 +1,8 @@
+package demo;
+
+public class HelloWorldJava {
+    public static void main(String[] args) {
+        System.out.println("Hello, world from Java!");
+        HelloWorldKtKt.main(args);
+    }
+}
